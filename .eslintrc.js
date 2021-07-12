22fd3d11d60d44386ddfb685f2e7e16e8b60c910
yona3 <kou.y.0905@icloud.com>
2021-07-13 00:34:34 +0900

    ✨ Add Linter

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      2,
+      { args: "all", argsIgnorePattern: "^_" },
+    ],
+  },
+};
